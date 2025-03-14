1cc212f6ddad49185b8525fe455327a7c289f0b9
Henry Li <henry1943@163.com>
2025-03-14 14:02:02 +0800

    feat: add link

--- src/app/_components/AppHeader.tsx
+++ src/app/_components/AppHeader.tsx
@@ -1,3 +1,13 @@
 export function AppHeader() {
-  return <div className="text-lg text-gray-500">LangManus</div>;
+  return (
+    <div>
+      <a
+        className="font-serif text-lg font-extralight text-gray-500"
+        href="https://github.com/langmanus/langmanus"
+        target="_blank"
+      >
+        LangManus
+      </a>
+    </div>
+  );
 }

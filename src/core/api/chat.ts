abe33a20ad271a42fb66f001698b86d3198ca9a7
Henry Li <henry1943@163.com>
2025-03-18 19:43:10 +0800

    chore: add default URL of API root

--- src/core/api/chat.ts
+++ src/core/api/chat.ts
@@ -11,15 +11,18 @@ export function chatStream(
   params: { deepThinkingMode: boolean; searchBeforePlanning: boolean },
   options: { abortSignal?: AbortSignal } = {},
 ) {
-  return fetchStream<ChatEvent>(env.NEXT_PUBLIC_API_URL + "/chat/stream", {
-    body: JSON.stringify({
-      messages: [...state.messages, userMessage],
-      deep_thinking_mode: params.deepThinkingMode,
-      search_before_planning: params.searchBeforePlanning,
-      debug:
-        location.search.includes("debug") &&
-        !location.search.includes("debug=false"),
-    }),
-    signal: options.abortSignal,
-  });
+  return fetchStream<ChatEvent>(
+    (env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000/api") + "/chat/stream",
+    {
+      body: JSON.stringify({
+        messages: [...state.messages, userMessage],
+        deep_thinking_mode: params.deepThinkingMode,
+        search_before_planning: params.searchBeforePlanning,
+        debug:
+          location.search.includes("debug") &&
+          !location.search.includes("debug=false"),
+      }),
+      signal: options.abortSignal,
+    },
+  );
 }

5cf1970dc1806ea9affa46b42a6db426d3b20638
Henry Li <henry1943@163.com>
2025-03-21 10:49:36 +0800

    feat: add copy button

--- src/app/_components/Markdown.tsx
+++ src/app/_components/Markdown.tsx
@@ -1,4 +1,9 @@
-import ReactMarkdown, { type Options } from "react-markdown";
+import { CheckOutlined, CopyOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import ReactMarkdown, {
+  type Options as ReactMarkdownOptions,
+} from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 import { cn } from "~/core/utils";
 
@@ -6,11 +11,20 @@ export function Markdown({
   className,
   children,
   style,
+  enableCopy,
   ...props
-}: Options & { className?: string; style?: React.CSSProperties }) {
+}: ReactMarkdownOptions & {
+  className?: string;
+  enableCopy?: boolean;
+  style?: React.CSSProperties;
+}) {
   return (
-    <div className={cn(className, "markdown")} style={style}>
+    <div
+      className={cn(className, "markdown flex flex-col gap-4")}
+      style={style}
+    >
       <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
         components={{
           a: ({ href, children }) => (
             <a href={href} target="_blank" rel="noopener noreferrer">
@@ -22,6 +36,38 @@ export function Markdown({
       >
         {children}
       </ReactMarkdown>
+      {enableCopy && typeof children === "string" && (
+        <div className="flex">
+          <CopyButton content={children} />
+        </div>
+      )}
     </div>
+  );
+}
+
+function CopyButton({ content }: { content: string }) {
+  const [copied, setCopied] = useState(false);
+  return (
+    <button
+      className="rounded-full bg-gray-50 px-4 py-2 text-sm text-gray-600 transition-all hover:bg-gray-100 hover:text-gray-900"
+      onClick={async () => {
+        try {
+          await navigator.clipboard.writeText(content);
+          setCopied(true);
+          setTimeout(() => {
+            setCopied(false);
+          }, 1000);
+        } catch (error) {
+          console.error(error);
+        }
+      }}
+    >
+      {copied ? (
+        <CheckOutlined className="h-4 w-4" />
+      ) : (
+        <CopyOutlined className="h-4 w-4" />
+      )}{" "}
+      {copied ? "Copied" : "Copy"}
+    </button>
   );
 }
